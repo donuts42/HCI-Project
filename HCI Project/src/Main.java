b55d0912104a07ab5fc19ef11a96285787f5c689
Jackson Blankenship <jacksblan@gmail.com>
2017-04-02 20:34:05 -0500

    Verify connectivity.
    Signed-off-by: Jackson Blankenship <jacksblan@gmail.com>

--- HCI Project/src/Main.java
+++ HCI Project/src/Main.java
@@ -6,10 +6,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		
-		System.out.println("World says hi");
-		//TESTING
 
 	}
 
